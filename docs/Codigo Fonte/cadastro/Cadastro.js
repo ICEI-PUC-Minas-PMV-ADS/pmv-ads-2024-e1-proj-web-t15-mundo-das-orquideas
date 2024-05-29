9701a43d345579c1a2476d17f2c4263bd4802b3a
AndrePeixot0 <163940524+AndrePeixot0@users.noreply.github.com>
2024-05-28 22:00:43 -0300

    a

--- docs/Codigo Fonte/cadastro/Cadastro.js
+++ docs/Codigo Fonte/cadastro/Cadastro.js
@@ -12,62 +12,37 @@ function cadastrar() {
     }
 
     if (telefone.value == "" || telefone.value.length < 11) {
-        alert{ "Preencha corretamente o telefone" };
+        alert("Preencha corretamente o telefone");
         telefone.focus();
         return;
 
     }
 
-    if (Senha.value == "" || senha.value.lengt < 6) {
-        alert("Preencha corretametne a senha")
+    if (senha.value == "" || senha.value.lengt < 6) {
+        alert("Preencha corretametne a senha");
         senha.focus();
         return;
     }
 
     if (confirmasenha.value !== senha.value) {
-        alert("Senhas não conferem")
+        alert("Senhas não conferem");
         confirmasenha.focus();
+        return;
 
     }
 
-.botao onclick function()[
-    alert 'ola;'
-]
-    let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]')
+    let lista = JSON.parse(localStorage.getItem('lista') || '[]')
+
+    lista.push(
+        {
+            nomeUser: nome.value,
+            emailUser: formulario.email.value,
+            telefoneUser: formulario.telefone.value,
+            SenhaUser: formulario.Senha.value,
+            ConfirmeSenhaUser: formulario.ConfirmeSenha.value
+        });
+        localStorage.setItem("lista", JSON.stringify(lista))
+
+        alert("Usuário cadastrado com sucesso!");
+        window.location.href= "../index.html"
 }
-
-
-
-
-// let form = document.getElementById('register-form');
-// let submit = document.getElementById('btn-submit');
-
-// //Cadastro de informações
-
-// document.getElementById('submit').addEventListener('click', function () {
-//     const nome = document.getElementById('nome').value;
-//     const email = document.getElementById('email').value;
-//     const telefone = document.getElementById('telefone').value;
-//     const senha = document.getElementById('senha').value;
-//     const confirmarsenha = document.getElementById('confirmarsenha').value;
-
-//     // Verifica se as senhas são iguais
-//     if (senha !== confirmarsenha) {
-//         alert('As senhas não coincidem.');
-//         return;
-//     }
-
-//     // Cria um objeto com os dados do usuário
-//     const userData = {
-//         nome: nome,
-//         email: email,
-//         telefone: telefone,
-//         senha: senha;
-//     };
-
-//     // Armazena os dados no localStorage
-//     localStorage.setItem('userData', JSON.stringify(userData));
-
-//     alert('Cadastro realizado com sucesso!');
-
-//     camponome = focus()
