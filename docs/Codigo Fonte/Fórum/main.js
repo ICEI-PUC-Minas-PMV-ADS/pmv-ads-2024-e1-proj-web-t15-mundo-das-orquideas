153716b239760a3218c9ed5cd7966ce9c685fd16
gustavoxpl <164693203+gustavoxpl@users.noreply.github.com>
2024-06-02 09:07:34 -0300

    Update main.js
    atualizacao

--- docs/Codigo Fonte/Fórum/main.js
+++ docs/Codigo Fonte/Fórum/main.js
@@ -24,3 +24,38 @@ function showReply(){
     var replyArea = document.getElementById("reply-area");
     replyArea.classList.remove("hide");
 }
+
+
+let topics = [];
+
+// Funcionalidade ao botão "Novo Tópico"
+document.getElementById("new-topic-btn").addEventListener("click", function() {
+    var question = prompt("Digite sua pergunta:");
+    if (question) {
+       
+        alert("Sua pergunta foi postada!");
+    }
+});
+
+
+function addQuestionToList(question) {
+    // Crie um novo elemento de lista
+    var listItem = document.createElement("li");
+    // Defina o texto da pergunta como o conteúdo do elemento de lista
+    listItem.textContent = question;
+    // Adicione o elemento de lista à lista de perguntas
+    document.getElementById("questions-list").appendChild(listItem);
+}
+
+document.getElementById("new-topic-btn").addEventListener("click", function() {
+    var question = prompt("Digite sua pergunta:");
+    if (question) {
+        addQuestionToList(question);
+        alert("Sua pergunta foi postada!");
+    }
+});
+// botão de responder
+document.querySelector('.reply-button').addEventListener('click', function() {
+    alert('Botão Responder clicado!'); // Exemplo: exibir um alerta
+   
+});
